11cf9a797b1a7d128721ce221839f56423449419
zsofiadeli <zsofia.deli@yahoo.com>
2024-10-05 19:17:43 +0200

    with_openapi - add openapi

--- src/main/java/org/prodet/security/controller/Controller.java
+++ src/main/java/org/prodet/security/controller/Controller.java
@@ -1,19 +1,29 @@
 package org.prodet.security.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import com.security.openapi.api.SecurityApi;
+import com.security.openapi.model.BubuPrivateModel;
+import com.security.openapi.model.BubuPublicModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Controller {
+public class Controller implements SecurityApi {
 
-    @GetMapping("/public")
-    public String getPublic() {
-        return "Public";
+    @Override
+    public ResponseEntity<BubuPrivateModel> getPrivate(BubuPrivateModel param) {
+        System.out.println("Incoming string: " + param.getTag());
+        Jwt jwt = (Jwt) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        System.out.println(jwt.getClaims());
+        return new ResponseEntity<>(param, HttpStatus.OK);
     }
 
-    @GetMapping("/private")
-    public String getPrivate() {
-        return "Private";
+    @Override
+    public ResponseEntity<BubuPublicModel> getPublic(BubuPublicModel param) {
+        System.out.println("Incoming string: " + param.getTag());
+        return new ResponseEntity<>(param, HttpStatus.OK);
     }
 
 }
